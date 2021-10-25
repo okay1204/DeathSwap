91219441f806b8a004d5681916dbe495a44d4566
okayyyy <zghan2005@gmail.com>
2021-10-24 21:52:45 -0700

    update swaptime text

--- src/me/okay/DeathSwap/CommandListener.java
+++ src/me/okay/DeathSwap/CommandListener.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
-import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -24,7 +23,7 @@ public class CommandListener implements CommandExecutor, TabCompleter {
         Map<String, String> commands = new TreeMap<String, String>();
         commands.put("start", "Starts the game of DeathSwap, where everyone currently in survival mode will be part of the game");
         commands.put("stop", "Ends the game of DeathSwap");
-        commands.put("settings swaptime <number>-<number>", "Sets the amount of time between swaps. The time will be a random time between the selected range");
+        commands.put("settings swaptime <minimum> <maximum>", "Sets the amount of minutes between swaps. The time will be a random time between the selected range");
         commands.put("settings fallkills <on|off>", "If on, players will be immune to fall damage after a teleport until they hit the ground");
         commands.put("settings lives <number>", "Sets the amount of lives each player has before they are eliminated");
         commands.put("help", "Displays this help menu");
@@ -35,7 +34,7 @@ public class CommandListener implements CommandExecutor, TabCompleter {
             helpMessage += "&7- &6/deathswap " + entry.getKey() + " &7- " + entry.getValue() + ".\n";
         }
 
-        helpMessage = deathSwap.toColorString(helpMessage);
+        helpMessage = DeathSwap.toColorString(helpMessage);
     }
 
     @Override
@@ -47,10 +46,10 @@ public class CommandListener implements CommandExecutor, TabCompleter {
             ArrayList<Player> players = deathSwap.getGame().getSurvivalPlayers();
 
             if (players.size() >= 2) {
-                // NOTE start game here
+                deathSwap.getGame().startGame(players);
             }
             else {
-                sender.sendMessage(deathSwap.toColorString("&cAt least 2 players are required to start the game. Make sure participants are in survival!"));
+                sender.sendMessage(DeathSwap.toColorString("&cAt least 2 players are required to start the game. Make sure participants are in survival!"));
             }
         }
         
